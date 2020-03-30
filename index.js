48e563a8040dcb96dbca69cdb6bf03a1175584b1
Frank Flanagan <flanaganfrank7@gmail.com>
2020-03-30 10:30:56 -0400

    updated form

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const styles = {
 	borderWidth:["0px 0px 1px 0px", "1px 1px 1px 1px", "0"],
-    backgroundColor:['none','lightgrey']
+	backgroundColor:['none','#F4F4F4'],
+	boxShadow: ['0px 10px 15px rgba(0, 0, 0, 0.1)', 'none']
 }
 
 
 let borderWidths = styles.borderWidth
 let bg = styles.backgroundColor
+let box = styles.boxShadow
 
 
 
@@ -13,7 +15,7 @@ let bg = styles.backgroundColor
 const f = (a, b) => [].concat(...a.map(d => b.map(e => [].concat(d, e))));
 const cartesian = (a, b, ...c) => (b ? cartesian(f(a, b), ...c) : a);
 
-let output = cartesian(borderWidths, bg);
+let output = cartesian(borderWidths, bg,box);
 console.log(output)
 
 
